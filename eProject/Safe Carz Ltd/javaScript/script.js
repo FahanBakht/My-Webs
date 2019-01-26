1f12dfa81b9a39a295f77dcea54f1dd34a460bfc
unknown <mfarhan.jp@gmail.com>
2019-01-26 12:44:26 +0500

    ADD: All my WEb

---
+++ eProject/Safe Carz Ltd/javaScript/script.js
@@ -0,0 +1,74 @@
+//javaScript
+
+
+
+//Chnage Color Of Vehicial
+	function selectVehiciel(vehicielDiv)
+	{
+		document.getElementById('z').style.backgroundColor = "rgb(186,12,47)";
+		document.getElementById('x').style.backgroundColor = "rgb(186,12,47)";
+		
+		vehicielDiv.style.backgroundColor = "#6D0318";
+	}
+
+//Chnage Color Of Brand
+	function selectbrand(brandDiv)
+	{
+		document.getElementById('a').style.backgroundColor = "rgb(186,12,47)";
+		document.getElementById('b').style.backgroundColor = "rgb(186,12,47)";
+		document.getElementById('c').style.backgroundColor = "rgb(186,12,47)";
+		
+		brandDiv.style.backgroundColor = "#6D0318";
+	}
+	
+//Chnage Color Of Plans
+	function selectPlan(planDiv)
+	{
+		document.getElementById('q').style.backgroundColor = "rgb(186,12,47)";
+		document.getElementById('w').style.backgroundColor = "rgb(186,12,47)";
+		document.getElementById('e').style.backgroundColor = "rgb(186,12,47)";
+		
+		planDiv.style.backgroundColor = "#6D0318";
+	}	
+
+// Function for Honda
+	function chngPriceDiamondhonda() {
+	
+		document.getElementById("diamond").innerHTML = "$800";
+		document.getElementById("gold").innerHTML = "$500";
+		document.getElementById("silver").innerHTML = "$250";
+	};
+
+// Function for Suzuki
+	function chngPriceDiamondsuzuki() {
+	
+		document.getElementById("diamond").innerHTML = "$650";
+		document.getElementById("gold").innerHTML = "$350";
+		document.getElementById("silver").innerHTML = "$150";
+	};
+
+// Function for Tata
+	function chngPriceDiamondtata() {
+	
+		document.getElementById("diamond").innerHTML = "$500";
+		document.getElementById("gold").innerHTML = "$250";
+		document.getElementById("silver").innerHTML = "$100";
+	};
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
